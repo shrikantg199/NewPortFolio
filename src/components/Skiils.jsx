3ba5ebfdfd236e8ab15d8af1fa9f60485920b16e
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-08 10:56:33 +0530

    initali commit

---
+++ src/components/Skiils.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import javascript from "../assets/javascript.png";
+import react from "../assets/React.png";
+
+import Mongodb from "../assets/Mongodb.png";
+import github from "../assets/github.png";
+import nodejs from "../assets/nodejs.png";
+import express from "../assets/expressjs.png";
+import tailwind from "../assets/tailwind.png";
+
+const Skills = () => {
+  const techs = [
+    {
+      id: 1,
+      src: html,
+      title: "HTML",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 2,
+      src: css,
+      title: "CSS",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 3,
+      src: javascript,
+      title: "JavaScript",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 4,
+      src: react,
+      title: "React",
+      style: "shadow-blue-600",
+    },
+    {
+      id: 5,
+      src: tailwind,
+      title: "Tailwind",
+      style: "shadow-sky-400",
+    },
+
+    {
+      id: 6,
+      src: Mongodb,
+      title: "Mongodb",
+      style: "shadow-white",
+    },
+    {
+      id: 7,
+      src: github,
+      title: "GitHub",
+      style: "shadow-gray-400",
+    },
+    {
+      id: 8,
+      src: nodejs,
+      title: "NodeJS",
+      style: "shadow-green-900",
+    },
+    {
+      id: 9,
+      src: express,
+      title: "ExpressJS",
+      style: "shadow-gray-400",
+    },
+  ];
+
+  return (
+    <div
+      name="Skills"
+      className="w-full h-auto bg-gradient-to-b from-gray-800 to-black"
+    >
+      <div className="flex flex-col justify-center w-full h-full max-w-screen-lg p-4 mx-auto text-white">
+        <div className="mt-20 mb-4 lg:mt-20">
+          <p className="inline p-2 text-4xl font-bold border-b-4 border-gray-500 ">
+            Skills
+          </p>
+          <p className="py-6">These are the technologies I've worked with</p>
+        </div>
+
+        <div className="grid w-full grid-cols-2 gap-8 px-12 py-8 text-center sm:grid-cols-3 sm:px-0">
+          {techs.map(({ id, src, title, style }) => (
+            <div
+              key={id}
+              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+            >
+              <img src={src} alt="" className="w-20 mx-auto" />
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
