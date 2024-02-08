7a0f44cd8adef295719da680ef366436d091cb14
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-08 10:54:33 +0530

    initali commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,45 +1,65 @@
 import React from "react";
-
+import { Input } from "@nextui-org/react";
 const Contact = () => {
+  const variants = ["bordered"];
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+      className="w-full h-auto p-4 text-white bg-gradient-to-b from-black to-gray-800 "
     >
-      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-        <div className="pb-8 lg:mt-28">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+      <div className="flex flex-col justify-center h-full max-w-screen-lg p-4 mx-auto mt-14 lg:mt-10 ">
+        <div className="pb-8 lg:mt-2">
+          <p className="inline text-4xl font-bold border-b-4 border-gray-500">
             Contact
           </p>
           <p className="py-6">Submit the form below to get in touch with me</p>
         </div>
 
-        <div className=" flex justify-center items-center">
+        <div className="flex items-center justify-center ">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            action="https://getform.io/f/66dd57e5-956b-4678-a35b-dbfa710b2277"
             method="POST"
-            className=" flex flex-col w-full md:w-1/2"
+            className="flex flex-col w-full gap-2  md:w-1/2"
           >
-            <input
-              type="text"
-              name="name"
-              placeholder="Enter your name"
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-            />
-            <input
-              type="text"
-              name="email"
-              placeholder="Enter your email"
-              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-            />
+            <div className="flex flex-col w-full gap-4">
+              {variants.map((variant) => (
+                <div
+                  key={variant}
+                  className="flex flex-wrap w-full gap-4 mb-6 md:flex-nowrap md:mb-0"
+                >
+                  <Input
+                    type="name"
+                    variant={variant}
+                    label="Name"
+                    className="border-white"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="flex flex-col w-full ">
+              {variants.map((variant) => (
+                <div
+                  key={variant}
+                  className="flex flex-wrap w-full gap-4 mb-6 md:flex-nowrap md:mb-0"
+                >
+                  <Input
+                    type="email"
+                    variant={variant}
+                    label="Email"
+                    className="border-white"
+                  />
+                </div>
+              ))}
+            </div>
+
             <textarea
               name="message"
               placeholder="Enter your message"
               rows="10"
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              className="p-2 text-white bg-transparent border-2 rounded-xl focus:outline-none"
             ></textarea>
 
-            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+            <button className="flex items-center px-6 py-3 mx-auto my-8 text-white duration-300 rounded-md bg-gradient-to-b from-cyan-500 to-blue-500 hover:scale-110">
               Let's talk
             </button>
           </form>
