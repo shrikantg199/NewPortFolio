ce0163b341b78b9593140c40afbc0a2b5169d917
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-08 10:57:15 +0530

    initali commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Experience from "./components/Experience";
+import Skills from "./components/Skiils";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
@@ -12,7 +12,7 @@ function App() {
       <Home />
       <About />
       <Portfolio />
-      <Experience />
+      <Skills />
       <Contact />
     </div>
   );
