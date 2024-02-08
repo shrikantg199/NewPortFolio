93eab51a130b35bef5d2865817c0f2c2afb056f0
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-08 10:54:55 +0530

    initali commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,16 +9,16 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+      className="w-full h-full bg-gradient-to-b from-black via-black to-gray-800"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full mt-20  mr-4">
-          <h2 className="text-4xl sm:text-7xl font-medium text-white">
-            I'm a Full Stack Developer.
+      <div className="flex flex-col items-center justify-center h-full max-w-screen-lg px-4 mx-auto md:flex-row ">
+        <div className="flex flex-col justify-center h-full mr-2 mt-60">
+          <h2 className="text-3xl font-medium text-white sm:text-7xl">
+            I'm a Full Stack Web Developer.
           </h2>
-          <p className="text-gray-500 py-4 max-w-md">
+          <p className="max-w-md py-4 text-gray-500 text-xl">
             I love to work on web application using technologies like React,
-            Tailwind, Next JS and GraphQL.
+            Tailwind, ExpressJS and MongoDB.
           </p>
 
           <div>
@@ -26,28 +26,28 @@ const Home = () => {
               to="portfolio"
               smooth
               duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+              className="flex items-center px-6 py-3 my-2 text-white rounded-md cursor-pointer group w-fit bg-gradient-to-r from-cyan-500 to-blue-500"
             >
               Portfolio
-              <span className="group-hover:rotate-90 duration-300">
+              <span className="duration-300 group-hover:rotate-90">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </Link>
             <div className="mt-4">
-              <div className="flex gap-4 mt-10">
+              <div className="flex gap-8 mt-10 mb-6">
                 <li className="list-none">
                   <a href="https://twitter.com/its_shri__11">
-                    <FaTwitter className="text-3xl text-white" />
+                    <FaTwitter className="text-4xl text-white hover:text-[#1DA1F2]" />
                   </a>
                 </li>
                 <li className="list-none">
                   <a href="https://www.linkedin.com/in/contactshrikantgaikwad/">
-                    <FaLinkedin className="text-3xl text-white " />
+                    <FaLinkedin className="text-4xl text-white " />
                   </a>
                 </li>
                 <li className="list-none">
                   <a href="https://instagram.com/its_shri_20">
-                    <LuInstagram className="text-3xl text-white" />
+                    <LuInstagram className="text-4xl instagram text-white rounded-md" />
                   </a>
                 </li>
               </div>
@@ -59,7 +59,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="blob  rounded-2xl mx-auto w-2/3 md:w-full "
+            className="w-[70%] mx-auto blob rounded-2xl md:w-full "
           />
         </div>
       </div>
