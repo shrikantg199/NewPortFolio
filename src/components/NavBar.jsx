96232348e22347c2bbbf3088cfb23e7d0dd97a45
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-21 11:16:25 +0530

    initial commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-scroll";
 
@@ -29,7 +29,7 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="fixed flex items-center justify-between w-full h-20 px-4 text-white bg-black">
+    <div className=" fixed flex items-center justify-between w-full h-20 px-4 text-white bg-black">
       <div>
         <Link to="home" smooth duration={500}>
           <h1 className="ml-2 text-5xl duration-200 cursor-pointer font-signature hover:scale-105 hover:text-orange-500">
@@ -58,12 +58,12 @@ const NavBar = () => {
       </li>{" "}
       <div
         onClick={() => setNav(!nav)}
-        className="z-10 pr-4 text-gray-500 cursor-pointer md:hidden"
+        className="z-10 pr-4 text-gray-500 cursor-pointer  md:hidden"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
       {nav && (
-        <ul className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-screen text-white bg-gradient-to-b from-black to-gray-800 ">
+        <ul className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-screen text-white bg-gradient-to-b from-black  to-gray-800 ">
           {links.map(({ id, link }) => (
             <li
               key={id}
